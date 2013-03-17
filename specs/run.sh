a4b5bab17c46d21ed9663a72afe43339bdb4a060
Gustavo Duarte <first@duartes.org>
2013-03-17 14:32:10 -0600

    Move tests around

--- specs/run.sh
+++ specs/run.sh
@@ -3,5 +3,4 @@
 SPECS=`dirname $0`
 MOCHA=$SPECS/../node_modules/mocha/bin/mocha
 
-$MOCHA --reporter spec --require should --compilers coffee:coffee-script $SPECS/sql/* $SPECS/access/* $SPECS/tds/* $SPECS/tsql/* $SPECS/schema/*
-#$SPECS/schema-loaded/* $SPECS/schema-mocked/*
+$MOCHA --reporter spec --require should --compilers coffee:coffee-script $SPECS/formatter/* $SPECS/access/* $SPECS/tds/* $SPECS/tsql/* $SPECS/schema/*
